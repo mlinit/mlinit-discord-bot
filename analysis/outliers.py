86679afaa3749a7ab442fe19a989cad6152feaff
Maharshi-Pandya <maharshi.pandya2002@gmail.com>
2023-02-04 16:03:53 +0530

    :hammer: little tweaks here and there

--- analysis/outliers.py
+++ analysis/outliers.py
@@ -6,12 +6,8 @@ from scipy import stats
 
 
 def z_score_out(df, col):
-    """
-        Counts the number of outliers for a specific
-        column of a dataframe using Z-score.
-    """
-    zscores_df = df[np.abs(stats.zscore(df[col]) < 3)][col]
-    num_outliers = df[col].shape[0] - zscores_df.shape[0]
+    zscores_col = df[np.abs(stats.zscore(df[col], nan_policy="omit") < 3)][col]
+    num_outliers = df[col].shape[0] - zscores_col.shape[0]
     
     return num_outliers
 
@@ -38,4 +34,46 @@ class Outliers:
         self.df = df
         
     def perform_zscore_out(self):
-        pass
+        """
+        Counts the number of outliers for a specific
+        column of a dataframe using Z-score.
+        """
+        num_features = [self.df.select_dtypes(include=["number"]).count()][0].index
+        num_features = list(num_features)
+        feats_len = len(num_features)
+        
+        zouts = np.zeros(shape=feats_len, dtype=np.int64)
+        
+        for i, col in enumerate(num_features):
+            zouts[i] = z_score_out(self.df, col)
+            
+        return (num_features, zouts)
+    
+    def perform_quantile_out(self):
+        """
+            Counts the number of outliers for a specific
+            column of a dataframe using quantile method.
+        """
+        num_features = [self.df.select_dtypes(include=["number"]).count()][0].index
+        num_features = list(num_features)
+        feats_len = len(num_features)
+        
+        upper_outliers = np.zeros(shape=feats_len, dtype=np.int64)
+        lower_outliers = np.zeros(shape=feats_len, dtype=np.int64)
+        
+        for i, feat in enumerate(num_features):
+            q1 = self.df[feat].quantile(q=0.25)
+            q3 = self.df[feat].quantile()
+            # interquantile range
+            iqr = q3 - q1
+            
+            lower_limit = q1 - (1.5 * iqr)
+            upper_limit = q3 + (1.5 * iqr)
+            
+            a = self.df[self.df[feat] < lower_limit].shape[0]
+            b = self.df[self.df[feat] > upper_limit].shape[0]
+            
+            lower_outliers[i] = a
+            upper_outliers[i] = b
+            
+        return (num_features, lower_outliers, upper_outliers)
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 
 from analysis import utils
 from analysis import describe
+from analysis import outliers
 
 # token for dc bot
 load_dotenv()
@@ -20,6 +21,7 @@ MSGLIMIT = 2000
 COLS_PER_PAGE = 7
 INFORMATION_THUMBNAIL_URL = "https://i.imgur.com/KY5QcLw.jpg"
 SUMMARY_THUMBNAIL_URL = "https://i.imgur.com/P1ZNLBa.jpg"
+OUTLIERS_THUMBNAIL_URL = "https://i.imgur.com/FmNH0qi.jpg"
 
 
 bot = commands.Bot(command_prefix="?", intents=discord.Intents.all())
@@ -59,7 +61,7 @@ async def information(ctx, url: str, user: discord.User):
         des = describe.Describe(resp)
         rows, feats, null, dtypes, memuse = des.perform_info()
         
-        await ctx.send(f"{ctx.message.author.mention} analyzing your data...🔃")
+        await ctx.reply(f"analyzing your data 🔃")
         
         info_embed_pages = []
         num_bins = math.ceil(len(feats) / COLS_PER_PAGE)
@@ -67,13 +69,14 @@ async def information(ctx, url: str, user: discord.User):
         for i in range(num_bins):
             embed = discord.Embed(
                 title="**dataset information** 📝",
-                description=f"here is the information related to\nthe dataset _{resp.name}_\n\nit contains **{rows}** rows and **{len(feats)}** columns...",
+                description=f"here is the information related to\nthe dataset _{resp.name}_\n\nit contains **{rows}** rows and **{len(feats)}** columns\n\n{'⎯'*10}\n",
                 color=discord.Color.dark_teal()
             )
             for j, fe in enumerate(feats[7*i: 7*(i+1)]):
+                ind = COLS_PER_PAGE*i + j
                 embed.add_field(
                     name=f"✅ **{fe}**",
-                    value=f"• {null[j]} null values\n• data type: {dtypes[j]}",
+                    value=f"• {null[ind]} null values\n• data type: {dtypes[ind]}",
                     inline=False
                 )
                 
@@ -93,7 +96,7 @@ async def information(ctx, url: str, user: discord.User):
             loop_pages=True,
         )
         
-        t_msg = f"{ctx.message.author.mention} check your DM for the result 😁"
+        t_msg = f"{user.mention} check your DM for the result ✔️"
         await info_paginator.send(ctx, target=user, target_message=t_msg)
         
             
@@ -115,7 +118,7 @@ async def description(ctx, url: str, user: discord.User):
         des = describe.Describe(resp)
         _, feats, count, uniq, top, freq, mean, std, mini, q25, q50, q75, maxi = des.perform_summ()
         
-        await ctx.send(f"{ctx.message.author.mention} analyzing your data...🔃")
+        await ctx.reply(f"analyzing your data 🔃")
         
         summ_embed_pages = []
         num_bins = math.ceil(len(feats) / COLS_PER_PAGE)
@@ -123,21 +126,22 @@ async def description(ctx, url: str, user: discord.User):
         for i in range(num_bins):
             embed = discord.Embed(
                 title="**dataset summary** 📝",
-                description=f"here is the summary related to\nthe dataset _{resp.name}_'s features\n\nit shows the statistical measures of **{len(feats)}** columns...",
+                description=f"here is the summary related to\nthe dataset _{resp.name}_'s features\n\nit shows the statistical measures of **{len(feats)}** columns\n\n{'⎯'*10}\n",
                 color=discord.Color.dark_teal()
             )
             for j, fe in enumerate(feats[7*i: 7*(i+1)]):
-                val = f"• count: {count[j]}\n"
-                val = val + f"• unique: {uniq[j]}\n" if not pd.isnull(uniq[j]) else val
-                val = val + f"• top: {top[j]}\n" if not pd.isnull(top[j]) else val
-                val = val + f"• frequency: {freq[j]}\n" if not pd.isnull(freq[j]) else val
-                val = val + f"• mean: {mean[j]}\n" if not pd.isnull(mean[j]) else val
-                val = val + f"• std: {std[j]}\n" if not pd.isnull(std[j]) else val
-                val = val + f"• minimum: {mini[j]}\n" if not pd.isnull(mini[j]) else val
-                val = val + f"• quantile 1 (25%): {q25[j]}\n" if not pd.isnull(q25[j]) else val
-                val = val + f"• quantile 2 (50%): {q50[j]}\n" if not pd.isnull(q50[j]) else val
-                val = val + f"• quantile 1 (75%): {q75[j]}\n" if not pd.isnull(q75[j]) else val
-                val = val + f"• maximum: {maxi[j]}\n" if not pd.isnull(maxi[j]) else val
+                ind = COLS_PER_PAGE*i + j
+                val = f"• count: {count[ind]}\n"
+                val = val + f"• unique: {uniq[ind]}\n" if not pd.isnull(uniq[ind]) else val
+                val = val + f"• top: {top[ind]}\n" if not pd.isnull(top[ind]) else val
+                val = val + f"• frequency: {freq[ind]}\n" if not pd.isnull(freq[ind]) else val
+                val = val + f"• mean: {mean[ind]}\n" if not pd.isnull(mean[ind]) else val
+                val = val + f"• std: {std[ind]}\n" if not pd.isnull(std[ind]) else val
+                val = val + f"• minimum: {mini[ind]}\n" if not pd.isnull(mini[ind]) else val
+                val = val + f"• quantile 1 (25%): {q25[ind]}\n" if not pd.isnull(q25[ind]) else val
+                val = val + f"• quantile 2 (50%): {q50[ind]}\n" if not pd.isnull(q50[ind]) else val
+                val = val + f"• quantile 1 (75%): {q75[ind]}\n" if not pd.isnull(q75[ind]) else val
+                val = val + f"• maximum: {maxi[ind]}\n" if not pd.isnull(maxi[ind]) else val
                 
                 embed.add_field(
                     name=f"✅ **{fe}**",
@@ -159,7 +163,7 @@ async def description(ctx, url: str, user: discord.User):
             loop_pages=True,
         )
         
-        t_msg = f"{ctx.message.author.mention} check your DM for the result 😁"
+        t_msg = f"{user.mention} check your DM for the result ✔️"
         await info_paginator.send(ctx, target=user, target_message=t_msg)
 
 
@@ -181,10 +185,10 @@ async def duplicates(ctx, url: str, user: discord.User):
         des = describe.Describe(resp)
         duprows, dupcols = des.perform_dupl()
         
-        await ctx.reply(f"analyzing your data...🔃")
+        await ctx.reply(f"analyzing your data 🔃")
         embed = discord.Embed(
             title="**duplicates in the dataset** ✌🏻",
-            description=f"here is the information related to\nthe duplicate rows in the dataset _{resp.name}_\n",
+            description=f"here is the information related to\nthe duplicate rows in the dataset _{resp.name}_\n\n{'⎯'*10}\n",
             color=discord.Color.dark_teal()
         )
         
@@ -208,13 +212,90 @@ async def duplicates(ctx, url: str, user: discord.User):
         embed.set_author(name="🤖 MLinit")
         embed.set_footer(text="analyzed by MLinit")
         
-        await ctx.send(f"{user.mention} check your DM for the result 😁")
+        await ctx.send(f"{user.mention} check your DM for the result ✔️")
         await user.send(embed=embed)
 
 @duplicates.error
 async def dup_error(ctx, err):
     if isinstance(err, commands.errors.MissingRequiredArgument):
         await ctx.reply("bruh, missing arguments for this command 🤨")
+
+
+# ----------------------- OUTLIERS --------------------------------------
+class OutlierView(discord.ui.View):
+    def __init__(self, ctx, user, df):
+        super().__init__()
+        self.df = df
+        self.ctx = ctx
+        self.user = user
+    
+    @discord.ui.select(
+        placeholder="Choose an option...",
+        min_values=1,
+        max_values=1,
+        options=[
+            discord.SelectOption(
+                label="z-score",
+                description="choose this for analyzing outliers using z-score"
+            ),
+            discord.SelectOption(
+                label="quantile",
+                description="choose this for analyzing outliers using quantiles"
+            )
+        ]
+    )
+    async def out_callback(self, select, interaction: discord.Interaction):
+        if select.values[0] == "z-score":
+            out = outliers.Outliers(self.df)
+            feats, zouts = out.perform_zscore_out()
+            
+            out_embed_pages = []
+            num_bins = math.ceil(len(feats) / COLS_PER_PAGE)
+        
+            await self.ctx.reply(f"analyzing your data 🔃")
+        
+            for i in range(num_bins):
+                embed = discord.Embed(
+                    title="**dataset outliers using Z-SCORE** 🚫",
+                    description=f"here are the outliers related to\nthe dataset _{self.df.name}_'s features\n\nit shows the outlier numbers of **{len(feats)}** columns using the **Z-score** test\n\n{'⎯'*10}\n",
+                    color=discord.Color.dark_teal()
+                )
+                for j, fe in enumerate(feats[COLS_PER_PAGE*i: COLS_PER_PAGE*(i+1)]):
+                    ind = COLS_PER_PAGE*i + j
+                    embed.add_field(
+                        name=f"✅ **{fe}**",
+                        value=f"• {zouts[ind]} outlier(s)\n",
+                        inline=False
+                    )
+                    
+                embed.set_thumbnail(url=OUTLIERS_THUMBNAIL_URL)
+                embed.set_author(name="🤖 MLinit")
+                embed.set_footer(text="analyzed by MLinit")
+                out_embed_pages.append(embed)
+                
+            out_paginator = pages.Paginator(
+                pages=out_embed_pages,
+                show_disabled=True,
+                show_indicator=True,
+                use_default_buttons=False,
+                custom_buttons=get_page_buttons(),
+                loop_pages=True,
+            )
+            
+            t_msg = f"{self.user.mention} check your DM for the result ✔️"
+            await out_paginator.send(self.ctx, target=self.user, target_message=t_msg)
+            await interaction.response.defer()
+
+
+@bot.command(name="out")
+async def outliers_analysis(ctx, url: str, user: discord.User):
+    resp = utils.read_dataset_from_url(url)
+    resp.name = url.split("/")[-1]
+    
+    if isinstance(resp, str):
+        await ctx.reply(resp)
+    else:
+        await ctx.reply("alright! tell me the method", view=OutlierView(ctx, user, resp))
 
 
 # driver code
